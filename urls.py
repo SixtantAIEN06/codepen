09ae2dbd7ba939f35396496a0c70b26fdf4bf34a
leo <leo91716@gmail.com>
2020-02-24 14:54:45 +0800

    first

---
+++ urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.index),
+    path('gallery/',views.gallery),
+    path('styletransfer/',views.styletransfer),
+    path('styletransfer2/',views.styletransfer2),
+    path('delmypic/',views.delmypic,name='uplaodfile'),
+    # path('styletransfer/{imgpath:imgpath}',views.styletransfer),
+    path('json/',views.json),
+    path('upload/',views.upload),
+    path('facerecognition/',views.facerecognition),
+    path('styletransfer/json/',views.json),
+    path('styletransfer/httpget/',views.httpget),
+    path('selected/',views.selected),
+    path('login/',views.login),
+    path('signup/',views.signup),
+    path('first/', views.first),
+    path('menu/', views.menu)
+
+]
